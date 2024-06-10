576929db2cf1a9ead705c8b2d84da04c1c26cda5
JWarchol <warcholljakub@gmail.com>
2024-06-10 21:56:55 +0200

    Fixed comments.

--- Examples/procedures.sql
+++ Examples/procedures.sql
@@ -1,50 +1,50 @@
-### AllUnpaidByCustomer
+-- ### AllUnpaidByCustomer
 
 SELECT * FROM UnpaidOrders
 
 EXEC AllUnpaidByCustomer 4
 
-### AssociateParticipantWithTrip
+-- ### AssociateParticipantWithTrip
 
-    // Nie da się bo wycieczka jest za 4 dni
+    -- Nie da się bo wycieczka jest za 4 dni
     EXEC AssociateParticipantWithTrip 1, 1
 
     SELECT TripOrderID, Trips.TripID, StartDate
     FROM TripOrders
     JOIN Trips ON TripOrders.TripID = Trips.TripID
 
-    // Nie da się bo taki uczestnik nie istnieje
+    -- Nie da się bo taki uczestnik nie istnieje
     EXEC AssociateParticipantWithTrip 40, 1
 
-    // Nie da się bo taki uczestnik już uczestniczy
+    -- Nie da się bo taki uczestnik już uczestniczy
     EXEC AssociateParticipantWithTrip 1, 12
 
-    // Nie da się bo już jest maksymalna liczba uczestników
+    -- Nie da się bo już jest maksymalna liczba uczestników
     EXEC AssociateParticipantWithTrip 1, 1
 
-    // Normalnie się da
+    -- Normalnie się da
     EXEC AssociateParticipantWithTrip 2, 12
 
-### AssociateParticipantWithAttraction
+-- ### AssociateParticipantWithAttraction
 
-    // Nie da się bo wycieczka jest za 4 dni
+    -- Nie da się bo wycieczka jest za 4 dni
     EXEC AssociateParticipantWithAttraction 1, 1
 
-    // Nie da się bo taki uczestnik nie istnieje
+    -- Nie da się bo taki uczestnik nie istnieje
     EXEC AssociateParticipantWithAttraction 40, 1
 
-    // Nie da się bo taki uczestnik już uczestniczy
+    -- Nie da się bo taki uczestnik już uczestniczy
     EXEC AssociateParticipantWithAttraction 8, 1
 
-    // Nie da się bo już jest maksymalna liczba uczestników
+    -- Nie da się bo już jest maksymalna liczba uczestników
     EXEC AssociateParticipantWithAttraction 1, 1
 
-    // Normalnie się da
+    -- Normalnie się da
     EXEC AssociateParticipantWithAttraction 1, 4
 
-### BuyTrip
+-- ### BuyTrip
 
-    // Nie można bo wycieczka jest za 4 dni
+    -- Nie można bo wycieczka jest za 4 dni
     EXEC BuyTrip 1,1,1,1
 
-### BuyAttraction
+-- ### BuyAttraction
